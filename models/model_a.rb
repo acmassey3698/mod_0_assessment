b9f46967f8196d0f7576bce97646214f4295c7f6
Andrew Massey <acmassey@ncsu.edu>
2021-06-11 11:11:01 -0400

    Add triceratops model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+triceratops = {
+  'name' => 'Kevin',
+  'predator' => 'Tyrannosaurus Rex',
+  'length_in_feet' => 30,
+  'weight_in_tons' => 5,
+  'scary' => true,
+  'extinct' => true,
+  'favorite_foods' => ['palms', 'ferns', 'cycads'],
+  'continents_inhabited' => ['North America', 'Asia']
+}
