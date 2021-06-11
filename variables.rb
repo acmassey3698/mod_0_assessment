38a11134885584daa3586fa2cd7fdb4a0f2ba8f3
Andrew Massey <acmassey@ncsu.edu>
2021-06-11 10:56:50 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+movie = 'Taxi Driver'
+is_entertaining = true
+
+movie = 'Matrix'
+
+num_4 = 7
+num_6 = 10
+
+product = num_4 * num_6
+
+favorite_foods = ['bananas', 'burritos', 'cheeseburgers']
+favorite_foods.shift
+
+p movie.size
