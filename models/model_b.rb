3555d523ccd14cabed41892e57bd92edfa879595
Andrew Massey <acmassey@ncsu.edu>
2021-06-11 11:19:25 -0400

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  'name' => 'Andrew Massey',
+  'home_state' => 'North Carolina',
+  'age' => 23,
+  'years_of_experience' => 0.33,
+  'motivated_to_learn' => true,
+  'knows_javascript' => false,
+  'current_skills' => ['creating variables', 'using git', 'creating methods'],
+  'other_hobbies' => ['reading', 'climbing', 'skateboarding', 'watching movies']  
+}
